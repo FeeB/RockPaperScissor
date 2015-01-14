a495d1c3fc295e108e7201b36f6dfb064d8cca97
FeeB <s0538335@htw-berlin.de>
2015-01-14 11:45:04 +0100

    no need for player setter

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -32,15 +32,7 @@ public class Controller {
 		return this.playerOne;
 	}
 	
-	public void setPlayerOne(Player playerOne) {
-		this.playerOne = playerOne;
-	}
-	
 	public Player getPlayerTwo() {
 		return this.playerTwo;
-	}
-	
-	public void setPlayerTwo(Player playerTwo) {
-		this.playerTwo = playerTwo;
 	}
 }
