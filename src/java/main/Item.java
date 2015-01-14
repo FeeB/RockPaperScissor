6b523dd6d24ab69765ddd21c79be4219b6de492d
FeeB <s0538335@htw-berlin.de>
2015-01-14 09:47:26 +0100

    Added Rock, Paper, Scissor Class

--- src/java/main/Item.java
+++ src/java/main/Item.java
@@ -1,5 +1,12 @@
 package java.main;
 
 public interface Item {
+	String name = "";
+	
+	public String getName();
+	public void setName(String name);
+	public int beatsScissor();
+	public int beatsRock();
+	public int beatsPaper();
 	
 }
---
+++ src/java/main/Paper.java
@@ -0,0 +1,25 @@
+package java.main;
+
+public class Paper implements Item{
+	private String name;
+	
+	public String getName() {
+		return "";
+	}
+	
+	public void setName(String name) {
+		
+	}
+	
+	public int beatsScissor() {
+		return 0;
+	}
+	
+	public int beatsRock() {
+		return 0;
+	}
+	
+	public int beatsPaper() {
+		return 0;
+	}
+}
