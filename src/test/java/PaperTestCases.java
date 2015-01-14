d7c46fe36e43ae7fec6c0713833ca98f10dad228
FeeB <s0538335@htw-berlin.de>
2015-01-14 10:02:39 +0100

    Paper TestCases

---
+++ src/test/java/PaperTestCases.java
@@ -0,0 +1,27 @@
+package test.java;
+
+import static org.junit.Assert.*;
+import main.java.Paper;
+
+import org.junit.Test;
+
+public class PaperTestCases {
+
+public Paper paper = new Paper();
+	
+	@Test
+	public void testBeatsRock() {
+		assertEquals(1, paper.beatsRock());
+	}
+	
+	@Test
+	public void testBeatsPaper() {
+		assertEquals(0, paper.beatsPaper());
+	}
+	
+	@Test
+	public void testBeatsScissor() {
+		assertEquals(-1, paper.beatsScissor());
+	}
+
+}
