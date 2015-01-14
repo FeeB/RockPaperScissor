a230890b3d2d7fdd5407d73e487afa86dacfda1c
FeeB <s0538335@htw-berlin.de>
2015-01-14 10:47:27 +0100

    implements Player methods

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -6,11 +6,29 @@ public class Player {
 	private Item selectedItem;
 	
 	public void selectItem(Item item) {
-		
+		this.selectedItem = item;
 	}
 	
-	public void selectItemRandomly() {
-
+	public void selectItemRandomly() throws Exception {
+		Random random = new Random();
+		int randomNumber = random.nextInt(3);
+		
+		switch (randomNumber) {
+		case 0:
+			this.selectedItem = new Rock();
+			break;
+			
+		case 1:
+			this.selectedItem = new Paper();
+			break;
+		
+		case 2:
+			this.selectedItem = new Scissor();
+			break;
+		
+		default:
+			throw new Exception("Unknown number" + randomNumber);
+		}
 	}
 	
 	public Item getSelectedItem() {
