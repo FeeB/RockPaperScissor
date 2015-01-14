c3e5bb9f1a0d61030ac99def306dd0eb9a8362cc
FeeB <s0538335@htw-berlin.de>
2015-01-14 11:16:50 +0100

    Implement Scissor methods

--- src/main/java/models/Scissor.java
+++ src/main/java/models/Scissor.java
@@ -7,7 +7,15 @@ public class Scissor implements Item {
 		return this.name;
 	}
 	
-	public int beatsItem(Item item) {
-		return 0;
+	public int beatsItem(Item item) throws Exception {
+		if (item.getName().equals("Scissor")) {
+			return 0;
+		} else if (item.getName().equals("Rock")) {
+			return -1;
+		} else if (item.getName().equals("Paper")) {
+			return 1;
+		} else {
+			throw new Exception("Unknown Item!");
+		}
 	}
 }
