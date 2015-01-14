43285506a0394413fbda28a138486849cf868183
FeeB <s0538335@htw-berlin.de>
2015-01-14 11:43:53 +0100

    two cases were missing scissor & rock and paper & rock

--- src/test/java/models/controller/ControllerTestCases.java
+++ src/test/java/models/controller/ControllerTestCases.java
@@ -3,6 +3,7 @@ package test.java.models.controller;
 import static org.junit.Assert.*;
 import main.java.controller.Controller;
 import main.java.models.Paper;
+import main.java.models.Rock;
 import main.java.models.Scissor;
 
 import org.junit.Before;
@@ -13,21 +14,35 @@ public class ControllerTestCases {
 	public Controller controller = new Controller();
 	public Scissor scissor = new Scissor();
 	public Paper paper = new Paper();
-	
-	@Before
-	public void beforeTestCase() {
-		controller.setSelectedItem(scissor, paper);
-	}
+	public Rock rock = new Rock();
 	
 	@Test
 	public void testSetSelectedItem() {
+		controller.setSelectedItem(scissor, paper);
+
 		assertEquals(controller.getPlayerOne().getSelectedItem(), scissor);
 		assertEquals(controller.getPlayerTwo().getSelectedItem(), paper);
 	}
 	
 	@Test
-	public void testCalculateWinner() {
+	public void testCalculateWinnerScissorPaper() throws Exception {
+		controller.setSelectedItem(scissor, paper);
+
 		assertEquals(controller.calculateWinner(), controller.getPlayerOne());
+	}
+	
+	@Test
+	public void testCalculateWinnerScissorRock() throws Exception {
+		controller.setSelectedItem(scissor, rock);
+
+		assertEquals(controller.calculateWinner(), controller.getPlayerTwo());
+	}
+	
+	@Test
+	public void testCalculateWinnerRockPaper() throws Exception {
+		controller.setSelectedItem(rock, paper);
+
+		assertEquals(controller.calculateWinner(), controller.getPlayerTwo());
 	}
 
 }
