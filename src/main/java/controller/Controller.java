cd5b19124834eea1d9b14b1706d0a6ff2f96cd1d
FeeB <s0538335@htw-berlin.de>
2015-01-14 11:44:04 +0100

    implements controller methods

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -8,18 +8,39 @@ public class Controller {
 	private Player playerTwo = new Player();
 	
 	public void setSelectedItem (Item itemPlayerOne, Item itemPlayerTwo) {
-		
+		this.playerOne.selectItem(itemPlayerOne);
+		this.playerTwo.selectItem(itemPlayerTwo);
 	}
 	
-	public Player calculateWinner(){
-		return null;
+	public Player calculateWinner() throws Exception{
+		switch (this.playerOne.getSelectedItem().beatsItem(this.playerTwo.getSelectedItem())) {
+		case 1:
+			return this.playerOne;
+			
+		case 0:
+			return null;
+			
+		case -1:
+			return this.playerTwo;
+			
+		default:
+			throw new Exception("Unknown winner!");
+		}
 	}
 	
 	public Player getPlayerOne() {
 		return this.playerOne;
 	}
 	
+	public void setPlayerOne(Player playerOne) {
+		this.playerOne = playerOne;
+	}
+	
 	public Player getPlayerTwo() {
 		return this.playerTwo;
+	}
+	
+	public void setPlayerTwo(Player playerTwo) {
+		this.playerTwo = playerTwo;
 	}
 }
