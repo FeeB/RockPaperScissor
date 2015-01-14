e7468329b88aa35afd3666a0061727ccb40b4f2d
FeeB <s0538335@htw-berlin.de>
2015-01-14 11:08:10 +0100

    added throw Exception for TestCase

--- src/test/java/PaperTestCases.java
+++ src/test/java/PaperTestCases.java
@@ -12,19 +12,19 @@ public class PaperTestCases {
 public Paper paper = new Paper();
 	
 	@Test
-	public void testBeatsRock() {
+	public void testBeatsRock() throws Exception {
 		Rock rock = new Rock();
 		assertEquals(1, paper.beatsItem(rock));
 	}
 	
 	@Test
-	public void testBeatsPaper() {
+	public void testBeatsPaper() throws Exception {
 		Paper paper = new Paper();
 		assertEquals(0, paper.beatsItem(paper));
 	}
 	
 	@Test
-	public void testBeatsScissor() {
+	public void testBeatsScissor() throws Exception {
 		Scissor scissor = new Scissor();
 		assertEquals(-1, paper.beatsItem(scissor));
 	}
